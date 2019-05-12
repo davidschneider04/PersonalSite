a3a79e7da172d3aa664c121c29165ca4bbe395b1
DavidSchneider, Coach & CEO <davidschneider04@gmail.com>
2019-05-12 14:09:27 -0600

    configure urls

---
+++ dsp/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from django.conf.urls import url 
+from . import views
+
+app_name = 'dsp'
+
+urlpatterns = [
+    path('', views.home, name='homeblank'),
+    path('home/', views.home, name='home'),
+    
+]
