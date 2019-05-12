cd44d678757326118706983dc5b4f2c9f606095a
DavidSchneider, Coach & CEO <davidschneider04@gmail.com>
2019-05-12 14:11:44 -0600

    fix views

--- dsp/views.py
+++ dsp/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.urls import reverse
+from django.http import HttpResponse, HttpResponseRedirect
 
-# Create your views here.
+def home(request):
+	context={}
+	return render(request, 'dsp/home.html', context)
