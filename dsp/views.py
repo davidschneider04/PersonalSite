fa70271c65b11df90959ec318b61b28cba633fb3
DavidSchneider, Coach & CEO <davidschneider04@gmail.com>
2019-06-03 14:40:55 -0600

    am i the baddie?

--- dsp/views.py
+++ dsp/views.py
@@ -10,9 +10,9 @@ def resume(request):
 	context={}
 	return render(request, 'dsp/resume.html', context)
 
-def davesdatadepot(request):
-	context={}
-	return render(request, 'dsp/davesdatadepot.html', {"context": context})
+#def davesdatadepot(request):
+#	context={}
+#	return render(request, 'dsp/davesdatadepot.html', {"context": context})
 
 def pyython(request):
 	context={}
@@ -35,18 +35,27 @@ def denver_zipcodes(request):
 	context={}
 	return render(request, 'dsp/Projects/denver_zipcodes.html', context)
 
+def create_project(pname, purl, desc):
+	return {'project':pname, 'reflink':purl, 'description': desc}
 
-#def davesdatadepot(request):
-#	projects = []
-#	#saywhen
-#	desc = "Do you have a favorite line from a favorite TV show or movie, but don't remember what episode or time it's from? Let SayWhen find the exact spot and link you to it."
-#	projects.append({'project':'SayWhen-- Movie/TV Scene Finder', 'reflink':'https://saywhen.app', 'description':desc})
-#	#mapping mountains
-#	desc = "Using Python with Folium to create an interactive map of the 100 highest peaks in Colorado."
-#	purl = "http://davidschneiderprojects.com/davesdatadepot/mapping_mountains/"
-#	projects.append({'project':'Mapping Colorado Mountain Peaks', 'reflink':purl, 'description':desc})
-#	#denver zipcodes
-#	desc = "An exploratory project I did for a Coursera assignment. Using Python with Folium to plot restaurants, income, etc., zoned by ZCTA."
-#	purl = "http://davidschneiderprojects.com/davesdatadepot/denver_zipcodes/"
-#	projects.append({'project':'Mapping & Modeling Denver Zipcodes', 'reflink':purl, 'description':desc})
-#    return render(request, 'dsp/davesdatadepot.html', {"context": projects})
+def davesdatadepot(request):
+	projects = []
+	#saywhen
+	pname = "SayWhen-- Movie/TV Scene Finder"
+	purl = "https://saywhen.app"
+	desc = "Do you have a favorite line from a favorite TV show or movie, but don't remember what episode or time it's from? Let SayWhen find the exact spot and link you to it."
+	project = create_project(pname, purl , desc)
+	projects.append(project)
+	#mapping mountains
+	pname = 
+	purl = "http://davidschneiderprojects.com/davesdatadepot/mapping_mountains/"
+	desc = "Using Python with Folium to create an interactive map of the 100 highest peaks in Colorado."
+	project = create_project(pname, purl , desc)
+	projects.append(project)
+	#denver zipcodes
+	pname = "Mapping & Modeling Denver Zipcodes"
+	purl = "http://davidschneiderprojects.com/davesdatadepot/denver_zipcodes/"
+	desc = "An exploratory project I did for a Coursera assignment. Using Python with Folium to plot restaurants, income, etc., zoned by ZCTA."
+	project = create_project(pname, purl , desc)
+	projects.append(project)
+    return render(request, 'dsp/davesdatadepot.html', {"context": projects})
