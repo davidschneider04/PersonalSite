65c658c946e42348b9f414065b537b845ece99c1
DavidSchneider, Coach & CEO <davidschneider04@gmail.com>
2019-06-05 11:31:02 -0600

    staticfiles are fucked

--- dsprojects/settings.py
+++ dsprojects/settings.py
@@ -120,7 +120,7 @@ ALLOWED_HOSTS = ['*']
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'dsprojects')
+STATIC_ROOT = os.path.join(BASE_DIR, 'dsprojects')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
