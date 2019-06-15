09facc8d765475ce5eb5281e9380bf66b4f30712
DavidSchneider, Coach & CEO <davidschneider04@gmail.com>
2019-06-15 00:17:09 -0600

    adding model back in

--- dsp/models.py
+++ dsp/models.py
@@ -3,6 +3,6 @@ from django.db import models
 # Create your models here.
 #from taggit.managers import TaggableManager
 
-#class Project(models.Model):
-#    # ... fields here
-#    tags = TaggableManager()
+class Project(models.Model):
+    # ... fields here
+    tags = TaggableManager()
