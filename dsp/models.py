6c1e2c2cf03deed24b94035106309ae739a1edb2
DavidSchneider, Coach & CEO <davidschneider04@gmail.com>
2019-06-15 00:19:32 -0600

    forgot the import

--- dsp/models.py
+++ dsp/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-#from taggit.managers import TaggableManager
+from taggit.managers import TaggableManager
 
 class Project(models.Model):
     # ... fields here
