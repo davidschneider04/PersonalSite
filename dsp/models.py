671001a5b7dedf9e3ce6cd6aa5167177f00c32b6
DavidSchneider, Coach & CEO <davidschneider04@gmail.com>
2019-06-14 23:57:58 -0600

    installed taggit, in reqs.txt

--- dsp/models.py
+++ dsp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+#from taggit.managers import TaggableManager
+
+#class Project(models.Model):
+#    # ... fields here
+#    tags = TaggableManager()
