06918bb41864a144e677468fcb8381e131e0fa26
DavidSchneider, Coach & CEO <davidschneider04@gmail.com>
2019-07-27 01:13:16 -0600

    uhhh why does it not work online

--- dsp/schedule_creator.py
+++ dsp/schedule_creator.py
@@ -3,6 +3,7 @@ import calendar
 from dateutil.relativedelta import relativedelta
 
 import ics
+import pytz
 
 py_days = list(calendar.Calendar().iterweekdays())
 fmt_days = list(calendar.day_name)
@@ -27,6 +28,7 @@ def get_events(week_num, day_of_week, date=datetime.date.today(), yearsout=10):
     assert isinstance(week_num, int) and week_num>=1 and week_num<=4, "week number out of range"
     assert isinstance(day_of_week, int) and day_of_week>=0 and day_of_week<=6, "day number ranges in python from 0-6"
     initdate = date
+    date = pytz.timezone('US/Mountain').localize(date)
     cal = ics.Calendar()
     date = floor_month(date)
     while date.year < 2029:
