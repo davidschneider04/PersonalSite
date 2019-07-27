6c241167f59ea6b1d68963f6b248c108f01318e2
DavidSchneider, Coach & CEO <davidschneider04@gmail.com>
2019-07-27 00:31:18 -0600

    getting there5

--- dsp/forms.py
+++ dsp/forms.py
@@ -7,8 +7,8 @@ from . import views
 from .models import PTFormInput
 
 class ParkingTicketForm(forms.ModelForm):
-    day_of_week = autocomplete.Select2ListChoiceField(choice_list=list(calendar.day_name), widget=autocomplete.ListSelect2(url='dsp:ptform-options-autocomplete'))
-    week_of_month = autocomplete.Select2ListChoiceField(choice_list=['1st','2nd','3rd','4th'], widget=autocomplete.ListSelect2(url='dsp:ptform-options-autocomplete'))
+    day_of_week = autocomplete.Select2ListChoiceField(choice_list=list(calendar.day_name))#, widget=autocomplete.ListSelect2(url='dsp:ptform-options-autocomplete'))
+    week_of_month = autocomplete.Select2ListChoiceField(choice_list=['1st','2nd','3rd','4th'])#, widget=autocomplete.ListSelect2(url='dsp:ptform-options-autocomplete'))
     class Meta:
         model = PTFormInput
         fields = ('day_of_week', 'week_of_month',)
