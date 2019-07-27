c9de21b465c10a3d2158907d16367128c0455cb9
DavidSchneider, Coach & CEO <davidschneider04@gmail.com>
2019-07-26 23:18:58 -0600

    starting parking tickets project

--- dsp/forms.py
+++ dsp/forms.py
@@ -6,12 +6,12 @@ from dal import autocomplete
 from . import views
 
 class ParkingTicketForm(forms.ModelForm):
-    day_of_week=autocomplete.Select2ListChoiceField(choice_list=list(calendar.day_name), widget=autocomplete.ListSelect2(url='dsp:ptform-options-autocomplete'))
-    week_of_month=autocomplete.Select2ListChoiceField(choice_list=['1st','2nd','3rd','4th'], widget=autocomplete.ListSelect2(url='dsp:ptform-options-autocomplete'))
+    day_of_week = autocomplete.Select2ListChoiceField(choice_list=list(calendar.day_name), widget=autocomplete.ListSelect2(url='dsp:ptform-options-autocomplete'))
+    week_of_month = autocomplete.Select2ListChoiceField(choice_list=['1st','2nd','3rd','4th'], widget=autocomplete.ListSelect2(url='dsp:ptform-options-autocomplete'))
     class Meta:
         model = PTFormInput
         fields = ('day_of_week', 'week_of_month',)
         widgets = {
-            'day_of_week': autocomplete.ListSelect2(url='dsp:ptform-options-autocomplete')
+            'day_of_week': autocomplete.ListSelect2(url='dsp:ptform-options-autocomplete'),
             'week_of_month': autocomplete.ListSelect2(url='dsp:ptform-options-autocomplete')
         }
