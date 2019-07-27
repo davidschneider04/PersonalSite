6688f43bb374ba265f481b3eee4d803426ae9df2
DavidSchneider, Coach & CEO <davidschneider04@gmail.com>
2019-07-26 22:38:43 -0600

    oops

--- dsp/views.py
+++ dsp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.urls import reverse
 from django.http import HttpResponse, HttpResponseRedirect
+from dal import autocomplete
 
 from . import schedule_creator
 
