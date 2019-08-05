0b43123296bbafe7e71e335c778c3b0eefd01bb5
DavidSchneider, Coach & CEO <davidschneider04@gmail.com>
2019-08-05 17:01:05 -0600

    converting to my own ics code

---
+++ dsp/ScheduleCreator.py
@@ -0,0 +1,36 @@
+import calendar
+import datetime
+
+def create_cal():
+    cal = ''
+    cal += "BEGIN:VCALENDAR\nVERSION:2.0\n"
+    return cal
+
+def finish_cal(cal):
+    cal += 'END:VCALENDAR'
+    return cal
+
+def make_event(cal, week_num, day_of_week, date=datetime.date.today()):
+    assert isinstance(week_num, int) and week_num>=1 and week_num<=4, "week number out of range"
+    #assert isinstance(day_of_week, int) and day_of_week>=0 and day_of_week<=6, "day number ranges in python from 0-6"
+    weeknum = str(week_num)
+    #date = floor_month(date)
+    #make event
+    event = 'BEGIN:VEVENT\nDTSTART;TZID=America/Denver:'
+    starttime = datetime.date.today().strftime('%Y%m%d') + 'T120000Z\n'
+    event += starttime 
+    rrule = 'RRULE:FREQ=MONTHLY;BYDAY='
+    #determine day
+    days = list(calendar.day_name)
+    days_abr = [str.upper(abr[:2]) for abr in list(calendar.day_abbr)]
+    days_of_week = dict(zip(days, days_abr))
+    form_day_abr = days_of_week[day_of_week]
+    day_of_month = str(week_num) + form_day_abr
+    rrule += day_of_month + '\n'
+    event += rrule
+    weeks_suffix = {1:'st', 2:'nd', 3:'rd', 4:'th'}
+    suffix = weeks_suffix[week_num]
+    summary = f'SUMMARY:Parking Alert: {week_num}{suffix} {day_of_week} of month.\n'
+    event += summary
+    event += 'END:VEVENT\n'
+    return event
--- dsp/views.py
+++ dsp/views.py
@@ -5,6 +5,7 @@ from dal import autocomplete
 
 from .forms import ParkingTicketForm
 from . import create_schedules
+from ScheduleCreator import create_cal, finish_cal, make_event
 
 def home(request):
 	context={}
@@ -79,6 +80,10 @@ def parking_tickets(request):
 		# check whether it's valid:
 		if form.is_valid():
 			# process the data in form.cleaned_data as required, redirect to a new URL:
+			sc = ScheduleCreator()
+			acal = sc.create_cal()
+			acal += sc.make_event(acal, request)
+			acal = finish_cal(acal)
 			cal = schedule_creator(request)
 			response = HttpResponse(cal, content_type='application/text charset=utf-8')
 			response['Content-Disposition'] = 'attachment; filename="parking_reminders.ics"'
