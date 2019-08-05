0089a235ffac0455e37f8bc233c3b92d2b5fa10e
DavidSchneider, Coach & CEO <davidschneider04@gmail.com>
2019-08-05 17:41:13 -0600

    fix arguments to method

--- dsp/ScheduleCreator.py
+++ dsp/ScheduleCreator.py
@@ -11,11 +11,9 @@ def finish_cal(cal):
     return cal
 
 def make_event(cal, week_num, day_of_week, date=datetime.date.today()):
-    import calendar
-    import datetime
     #assert isinstance(week_num, int) and week_num>=1 and week_num<=4, "week number out of range"
     #assert isinstance(day_of_week, int) and day_of_week>=0 and day_of_week<=6, "day number ranges in python from 0-6"
-    weeknum = str(week_num)[0]
+    week_of_month = str(week_num)[0]
     #date = floor_month(date)
     #make event
     event = 'BEGIN:VEVENT\nDTSTART;TZID=America/Denver:'
@@ -27,12 +25,10 @@ def make_event(cal, week_num, day_of_week, date=datetime.date.today()):
     days_abr = [str.upper(abr[:2]) for abr in list(calendar.day_abbr)]
     days_of_week = dict(zip(days, days_abr))
     form_day_abr = days_of_week[day_of_week]
-    day_of_month = str(week_num) + form_day_abr
+    day_of_month = week_of_month + form_day_abr
     rrule += day_of_month + '\n'
     event += rrule
-    weeks_suffix = {1:'st', 2:'nd', 3:'rd', 4:'th'}
-    suffix = weeks_suffix[week_num]
-    summary = f'SUMMARY:Parking Alert: {week_num}{suffix} {day_of_week} of month.\n'
+    summary = f'SUMMARY:Parking Alert: {week_num} {day_of_week} of month.\n'
     event += summary
     event += 'END:VEVENT\n'
     return event
