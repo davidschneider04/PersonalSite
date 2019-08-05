8f2e70f031c2b63b24fb74ab26838c532faf03e6
DavidSchneider, Coach & CEO <davidschneider04@gmail.com>
2019-08-05 17:59:06 -0600

    fix arguments to method...

--- dsp/ScheduleCreator.py
+++ dsp/ScheduleCreator.py
@@ -10,7 +10,7 @@ def finish_cal(cal):
     cal += 'END:VCALENDAR'
     return cal
 
-def make_event(cal, week_num, day_of_week, date=datetime.date.today()):
+def make_event(week_num, day_of_week, date=datetime.date.today()):
     #assert isinstance(week_num, int) and week_num>=1 and week_num<=4, "week number out of range"
     #assert isinstance(day_of_week, int) and day_of_week>=0 and day_of_week<=6, "day number ranges in python from 0-6"
     week_of_month = str(week_num)[0]
@@ -24,7 +24,6 @@ def make_event(cal, week_num, day_of_week, date=datetime.date.today()):
     days = list(calendar.day_name)
     days_abr = [str.upper(abr[:2]) for abr in list(calendar.day_abbr)]
     days_of_week = dict(zip(days, days_abr))
-    days_of_week = {'Monday': 'MO', 'Tuesday': 'TU', 'Wednesday': 'WE', 'Thursday': 'TH', 'Friday': 'FR', 'Saturday': 'SA', 'Sunday': 'SU'}
     form_day_abr = days_of_week[day_of_week]
     day_of_month = week_of_month + form_day_abr
     rrule += day_of_month + '\n'
