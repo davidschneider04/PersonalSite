752f581565a29befb98638615a4282334eb35039
DavidSchneider, Coach & CEO <davidschneider04@gmail.com>
2019-08-05 18:48:48 -0600

    add alarm

--- dsp/ScheduleCreator.py
+++ dsp/ScheduleCreator.py
@@ -12,9 +12,13 @@ def finish_cal(cal):
 
 def make_event(week_num, day_of_week, date=datetime.date.today()):
     week_of_month = str(week_num)[0]
-    event = 'BEGIN:VEVENT\nDTSTART;TZID=America/Denver:'
-    starttime = datetime.date.today().strftime('%Y%m%d') + '\n' #all day
+    event = 'BEGIN:VEVENT\n'
+    starttime = 'DTSTART;TZID=America/Denver:'
+    starttime += datetime.date.today().strftime('%Y%m%d') + 'T080000Z\n'
     event += starttime
+    endtime = 'DTEND;TZID=America/Denver:'
+    endtime += datetime.date.today().strftime('%Y%m%d') + 'T170000Z\n'
+    event += endtime
     rrule = 'RRULE:FREQ=MONTHLY;BYDAY='
     days = list(calendar.day_name)
     days_abr = [str.upper(abr[:2]) for abr in list(calendar.day_abbr)]
@@ -24,6 +28,9 @@ def make_event(week_num, day_of_week, date=datetime.date.today()):
     rrule += day_of_month + '\n'
     event += rrule
     summary = f'SUMMARY:Parking Alert: {week_num} {day_of_week} of month.\n'
+    #alarm at 9PM and 7AM
+    alarm = 'BEGIN: VALARM\n' + 'TRIGGER:-PT11H\n' + 'REPEAT:1\n' + 'DURATION:PT10H\n' + 'ACTION:DISPLAY\n' + f'SUMMARY:{summary}\n' + 'END:VALARM\n'
+    event += alarm
     event += summary
     event += 'END:VEVENT\n'
     return event
