078cb427064751cb3b839f5290f451d747b476c0
DavidSchneider, Coach & CEO <davidschneider04@gmail.com>
2019-11-08 19:39:36 -0700

    add nfl dashboard

--- dsp/urls.py
+++ dsp/urls.py
@@ -37,6 +37,5 @@ urlpatterns = [
         views.PTFormInputAutocompleteDays.as_view(),
         name='ptform-options-autocomplete',
     ),
-    path('davesdatadepot/nfl/',
-        views.nfl_play_breakdown, name='nfl_play_breakdown'),
+
 ]
