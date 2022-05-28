409c55acdd9a17c7e458e58404da7543e1101b1d
root <david@davidtschneider.com>
2022-05-28 21:31:37 +0100

    we seem to have a working configuration

--- app/__init__.py
+++ app/__init__.py
@@ -2,20 +2,30 @@ from flask import Flask, render_template, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 
-app = Flask(__name__, template_folder='templates') # this is our "WSGI application"
+app = Flask(__name__, template_folder='html/templates') # this is our "WSGI application
 app.config.from_object('config')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
 
-@app.route('/plants/')
+@app.route("/")
+def index():
+    return render_template(f'base.html')
+
+@app.route('/amanda')
+def lam():
+    return render_template('amanda.html')
+
+@app.route('/plants')
 def plants():
     return render_template('plants.html')
 
-@app.route("/")
-def index():
-    return render_template(f'index.html')
+
+@app.route('/<pagenm>')
+def please(pagenm):
+    return render_template(f'{pagenm}.html')
+
 
 # blueprints
 #from app.auth.controllers import auth as auth_module
