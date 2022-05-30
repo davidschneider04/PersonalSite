784193b242be887e077518c994639d0142275449
root <david@davidtschneider.com>
2022-05-30 23:08:43 +0100

    taking the cheaters way out

--- app/__init__.py
+++ app/__init__.py
@@ -16,12 +16,12 @@ db = SQLAlchemy(app)
 def home():
     return render_template('home.html')
 
-"""
+
 @app.route('/davesdatadepot/<project>')
 def singleproj(project):
     return render_template([f'{project}.html', '404.html'])
 
-"""
+
 @app.route('/davesdatadepot')
 def projects():
     def create_project(pname, purl, desc):
@@ -29,17 +29,17 @@ def projects():
     projects = []
     projects.append(create_project(
         "FindScene — App to Find Video Links",
-        "davesdatadepot/findscene",
+        "findscene",
         "Do you have a favorite line from a favorite TV show or movie, but don't remember what episode or time it's from? Let FindScene find the exact spot and link you to it."
         ))
     projects.append(create_project(
         "Mapping Colorado Mountains",
-        "davesdatadepot/mapping_mountains",
+        "mapping_mountains",
         "Using Python with Folium to create an interactive map of the 100 highest peaks in Colorado."
         ))
     projects.append(create_project(
         "Code For This Website",
-        "davesdatadepot/website_code",
+        "website_code",
         "So meta. Tools and strategies used for deploying and hosting."
         ))
     return render_template('davesdatadepot.html', context=projects)
